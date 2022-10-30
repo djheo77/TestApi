03045d8f1e9a1ee87320729bb1bac713b96cab20
djheo-carbon <djheo77@gmail.com>
2022-10-30 15:40:11 +0900

    first github commit & push

---
+++ Console/Console/Console.cpp
@@ -0,0 +1,12 @@
+// Console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
+
